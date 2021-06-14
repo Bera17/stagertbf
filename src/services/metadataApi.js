22d6403d193571171d71793db183080d96777183
Bera17 <43541945+Bera17@users.noreply.github.com>
2021-06-14 18:32:22 +0200

    Passage au https

--- src/services/metadataApi.js
+++ src/services/metadataApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:3100/api/metadata",
+    baseURL: "https://localhost:3100/api/metadata",
   });
 
   const retrieve = () => {
